b7f798fa3e7c43e361d14212fd0b8ac1c41ca3a8
Andrew Low <andrew.low@inspection.gc.ca>
2017-11-22 13:02:04 -0500

    Make a failing build

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class Test(unittest.TestCase):
         self.assertTrue(is_even(4))
 
     def test_false(self):
-        self.assertFalse(is_even(5))
+        self.assertTrue(is_even(5))
 
 
 if __name__ == '__main__':
