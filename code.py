b12aa1cea0963da721ca27f1dea1200d23e8fa4d
Andrew Low <andrew.low@inspection.gc.ca>
2017-11-22 11:53:57 -0500

    First commit

---
+++ code.py
@@ -0,0 +1,7 @@
+from Bio import SeqIO
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
---
+++ tests.py
@@ -0,0 +1,14 @@
+import unittest
+from code import is_even
+
+
+class Test(unittest.TestCase):
+    def test_even(self):
+        self.assertTrue(is_even(4))
+
+    def test_false(self):
+        self.assertFalse(is_even(5))
+
+
+if __name__ == '__main__':
+    unittest.main()
