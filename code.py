7d382edf3e612a05e45dbceb00576b6eea2731ca
Andrew Low <andrew.low@inspection.gc.ca>
2017-11-22 13:11:08 -0500

    Trying to add external dependency

--- code.py
+++ code.py
@@ -1,7 +1,16 @@
 from Bio import SeqIO
+import shutil
+
 
 def is_even(num):
     if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def dependency_check(program):
+    if shutil.which(program) is not None:
         return True
     else:
         return False
--- tests.py
+++ tests.py
@@ -1,13 +1,16 @@
 import unittest
 from code import is_even
-
+from code import dependency_check
 
 class Test(unittest.TestCase):
     def test_even(self):
         self.assertTrue(is_even(4))
 
     def test_false(self):
-        self.assertTrue(is_even(5))
+        self.assertFalse(is_even(5))
+
+    def test_dependency(self):
+        self.assertTrue(dependency_check('bcftools'))
 
 
 if __name__ == '__main__':
